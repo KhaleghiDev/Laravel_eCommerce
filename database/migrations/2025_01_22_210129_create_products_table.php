06bdeb85a94192488efc2a73067722cc4090801b
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2025-01-23 20:34:10 +0330

    first commit add migration first

---
+++ database/migrations/2025_01_22_210129_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->foreignId('brand_id');
+            $table->foreign('brand_id')->on('brands')->onDelete('cascade');
+
+            $table->foreignId('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('primary_image');
+            $table->text('description');
+            $table->integer('status')->default(1);
+            $table->boolean('is_active')->default(1);
+            $table->unsignedBigInteger('delivery_amount')->default(1);
+            $table->unsignedBigInteger('delivery_amount_per_product')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productess');
+    }
+};
